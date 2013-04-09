b7a18c13f40634215085d8552048cb5bf6c0eea9
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2013-04-09 13:50:19 +0900

    suport callout style (fixed #5)

--- scripts/html2mkd.py
+++ scripts/html2mkd.py
@@ -93,7 +93,7 @@ def write_blog(data):
         msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
     out = sys.stdout
     out.write('''---
-layout: ja-doc
+layout: en-doc
 title: %(title)s
 summary: %(summary)s
 #sort-priority: 0
