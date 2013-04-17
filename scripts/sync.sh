475d98979ba9f5ec7bf90e5abcf2b21f8a8ce591
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2013-04-17 17:50:08 +0900

    set permission explicitly

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -rlptDv --delete-after _site/ "kiidocs@docs101us.internal.kii.com:/ext/ebs/html"
+rsync -rlptDv --chmod=u+rw,g+r,o+r --chmod=Da+x --delete-after _site/ "kiidocs@docs101us.internal.kii.com:/ext/ebs/html"
