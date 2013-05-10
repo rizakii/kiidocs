6d45d8699fe7be5518cd928923abdb3fcdd278fd
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2013-05-10 15:43:37 +0900

    upload docs102us too

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-rsync -rlptDv --chmod=u+rw,g+r,o+r --chmod=Da+x --delete-after _site/ "kiidocs@docs101us.internal.kii.com:/ext/ebs/html"
+dests="kiidocs@docs101us.internal.kii.com:/ext/ebs/html kiidocs@docs102us.internal.kii.com:/ext/ebs/html"
+
+for d in $dests ; do
+  echo "Uploading to: $d"
+  rsync -rlptD --chmod=u+rw,g+r,o+r --chmod=Da+x --delete-after --progress _site/ "$d"
+  echo "...uploaded"
+  echo ""
+  echo ""
+done
