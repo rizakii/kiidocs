b7492cd7af357f8c97dd76039dea4e8b672eb852
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2013-05-27 13:55:34 +0900

    use class operation to control left navi

--- assets/javascripts/sidebar-autoscroll.js
+++ assets/javascripts/sidebar-autoscroll.js
@@ -1,25 +1,103 @@
 // vim:set ts=8 sts=2 sw=2 tw=0 et:
+
 $(function() {
+  var FIXED = 'fixed';
+  var BOTTOM = 'bottom';
+  var SCROLLABLE = 'scrollable';
+  var CHROME_FIX = 'chrome-fix';
   var sidebar = $('.sidebar');
-  if (sidebar.length > 0) {
-    setupSidebarScroll(sidebar);
+  var parent = sidebar.parent();
+  var begin = sidebar.offset().top;
+
+  var requiredHeight = sidebar.height();
+
+  function getEnd() {
+    return parent.offset().top + parent.height() - sidebar.height();
   }
 
-  function setupSidebarScroll(target) {
-    var parent = target.parent();
-    var targetTop = target.offset().top;
-    $(window).scroll(function() {
-      var scrollTop = $(window).scrollTop();
-      var offset = 0;
-      var max = parent.height() - target.height();
-      if ($(window).height() > target.height() && scrollTop > targetTop) {
-        offset = scrollTop - targetTop;
-        if (offset > max) {
-          offset = max;
-        }
+  function getNextMode() {
+    var mode = 0;
+    var curr = $(window).scrollTop();
+    if (curr > begin) {
+      mode = 1;
+      if (curr > getEnd()) {
+        mode = 2;
       }
-      //console.log('offset=%d scrollTop=%d', offset, scrollTop);
-      target.offset({ top: targetTop + offset });
-    });
+    }
+    return mode;
   }
+
+  function getCurrMode() {
+    if (sidebar.hasClass(BOTTOM)) {
+      return 2;
+    } else if (sidebar.hasClass(FIXED)) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  function applyMode(mode) {
+    switch (mode) {
+      default:
+      case 0:
+        sidebar.removeClass(FIXED);
+        sidebar.removeClass(BOTTOM);
+        break;
+      case 1:
+        sidebar.addClass(FIXED);
+        sidebar.removeClass(BOTTOM);
+        break;
+      case 2:
+        sidebar.removeClass(FIXED);
+        sidebar.addClass(BOTTOM);
+        break;
+    }
+  }
+
+  function checkScroll() {
+    var nextMode = getNextMode();
+    if (nextMode != getCurrMode()) {
+      applyMode(nextMode);
+    }
+  }
+
+  function chromeAdjustWidth() {
+    if (navigator.userAgent.indexOf('Chrome')) {
+      var children = sidebar.children();
+      children.toggleClass(CHROME_FIX, true);
+      setTimeout(function() {
+        children.toggleClass(CHROME_FIX, false);
+      }, 10);
+    }
+  }
+
+  function checkHeight() {
+    var isScrollable = sidebar.hasClass(SCROLLABLE) ? true : false;
+    var h = $(window).height();
+    if (h < requiredHeight) {
+      sidebar.height(h);
+      if (!isScrollable) {
+        sidebar.toggleClass(SCROLLABLE, true);
+      }
+    } else {
+      if (isScrollable) {
+        sidebar.height(requiredHeight);
+        sidebar.toggleClass(SCROLLABLE, false);
+        // chrome workaround: short width after removing scrollbar.
+        chromeAdjustWidth();
+      }
+    }
+  }
+
+  $(window).scroll(function() {
+    checkScroll();
+  });
+
+  $(window).resize(function() {
+    checkHeight();
+    checkScroll();
+  });
+
+  checkHeight();
 });
